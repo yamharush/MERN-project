da131e196f2f364ce8709e8dff5c1531434c1377
yamharush <73635032+yamharush@users.noreply.github.com>
2022-11-15 20:15:27 +0200

    second commit

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,27 +1,49 @@
 
+const request = require('supertest')
+const app = require('../server')
+const mongoose = require('mongoose')
+const Post = require('../models/post_model')
 
-beforeAll(done => {
-    done()
+
+const newPostMessage = 'This is the new test post message'
+const newPostSender = '999000'
+let newPostId = ''
+
+
+beforeAll(async () => {
+    await Post.remove()
 })
 
-afterAll(done => {
-    done()
+afterAll(async () => {
+    await Post.remove()
+    mongoose.connection.close()
 })
 
 describe("Posts Tests", () => {
-    test("add new post", async () => {
-        const tmp = 2
-        expect(tmp).toEqual(2)
-    })
 
     test("add new post", async () => {
-        const tmp = 2
-        expect(tmp).toEqual(2)
+        const response = await request(app).post('/post').send({
+            "message": newPostMessage,
+            "sender": newPostSender
+        })
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.message).toEqual(newPostMessage)
+        expect(response.body.sender).toEqual(newPostSender)
+        newPostId = response.body._id
     })
 
-    test("add new post", async () => {
-        const tmp = 2
-        expect(tmp).toEqual(3)
+    test("get all posts", async () => {
+        const response = await request(app).get('/post')
+        expect(response.statusCode).toEqual(200)
+        expect(response.body[0].message).toEqual(newPostMessage)
+        expect(response.body[0].sender).toEqual(newPostSender)
+    })
+
+    test("get post by id", async () => {
+        const response = await request(app).get('/post/' + newPostId)
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.message).toEqual(newPostMessage)
+        expect(response.body.sender).toEqual(newPostSender)
     })
 })
 
